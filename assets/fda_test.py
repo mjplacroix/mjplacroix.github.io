e2895d2005e6d12919c45b3ead1b53bf062fbd76
Michael LaCroix <michael.jp.lacroix@gmail.com>
2022-04-26 09:15:08 -0400

    more edits to fda api

--- assets/fda_test.py
+++ assets/fda_test.py
@@ -3,7 +3,6 @@ pload = {'username':'Olivia','password':'123'}
 r = requests.post('https://httpbin.org/post',data = pload)
 print(r.text)
 
-
 # defining the api-endpoint
 API_ENDPOINT = "https://api-datadashboard.fda.gov/v1/inspections_classifications"
 
@@ -13,28 +12,32 @@ data = {'Content-Type': "application/json",
         'Authorization-Key': "WIBEC7KSOAABYID"
         }
 
+json = """
+{
+    "start": 1,
+    "rows": 10,
+    "sort": "LegalName",
+    "sortorder": "ASC",
+    "returntotalcount": true,
+    "filters": {
+        "InspectionEndDateFrom": [
+            "2001-11-27"
+        ],
+        "InspectionEndDateTo": [
+            "2020-11-27"
+        ]
+    },
+    "columns": [
+        "FEINumber",
+        "LegalName",
+        "CitationID",
+        "InspectionID",
+        "InspectionEndDate"
+    ]
+}
+        """
 # sending post request and saving response as response object
-r = requests.post(API_ENDPOINT, data = data, json=)
+r = requests.post(API_ENDPOINT, data = data, json=json)
 
 # extracting response text
 print(r.text) 
-
-
-json = """
-        {
-            "start" : 1,
-            "rows" : 10,
-            "sort" : "ProductCode",
-            "sortorder" : "ASC",
-            "filters" : {
-                "FEINumber":[3003378587,1000117386,3008091479]
-            },
-            "columns" : [
-                "FEINumber",
-                "FirmName",
-                "CountryCode",
-                "ProductCode",
-                "RefusalDate"
-            ]
-        }
-        """
