d7a21f453b47589658251cc2aa95bbc6bdd17815
Michael LaCroix <michael.jp.lacroix@gmail.com>
2022-04-29 09:08:20 -0400

    api now returning data - difficulty parsing

--- assets/fda_test.py
+++ assets/fda_test.py
@@ -1,41 +1,43 @@
 import requests
+import pandas as pd
+import json
 
 # defining the api-endpoint
-FDA_endpoint = "https://api-datadashboard.fda.gov/v1/inspections_classifications"
+FDA_endpoint = "https://api-datadashboard.fda.gov/v1/inspections_classifications"      
 
 # data to be sent to api
 data = {'Content-Type': "application/json",
-        'Authorization-User': "michael.jp.lacrox@gmail.com",
+        'Authorization-User': "michael.jp.lacroix@gmail.com",
         'Authorization-Key': "WIBEC7KSOAABYID"
         }       
 
 json = """
-{
-    "start": 4,
-    "rows": 10,
-    "sort": "LegalName",
-    "sortorder": "ASC",
-    "returntotalcount": true,
-    "filters": {
-        "InspectionEndDateFrom": [
-            "2001-11-27"
-        ],
-        "InspectionEndDateTo": [
-            "2020-11-27"
-        ]
+{  "start" : 1,
+    "rows" : 10,
+    "sort" : "InspectionEndDate",
+    "sortorder" : "ASC",
+    "filters" : {
+        "InspectionEndDateFrom":["2001-11-27"],
+        "InspectionEndDateTo":["2020-11-27"]
     },
-    "columns": [
+    "columns" : [
         "FEINumber",
         "LegalName",
-        "CitationID",
         "InspectionID",
+        "Classification",
         "InspectionEndDate"
     ]
 }
         """
-
 # sending post request and saving response as response object
-r = requests.post(FDA_endpoint, data=data, json=json)
+r = requests.post(FDA_endpoint, headers=data, json=json)
 
 # extracting response text
-print(r.text) 
+# print(r.text)
+
+# data = r.json()
+# print(data)
+
+json_data = json.loads(r.text)
+
+print(json_data)
